40869a2c660e7326d63b739117ca10cc7eeaae68
eshaan <eshaanbagga@gmail.com>
2024-02-13 15:26:33 +0530

    done with ui and validation of authentication

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,6 +1,11 @@
-app.controller('LoginController', function($scope, $location) {
-    // Controller logic for login page
-    $scope.goToSignup = function() {
-        $location.path('/signup');
-    };
+app.controller("LoginController", function ($scope, $location) {
+  // Controller logic for login page
+  $scope.goToSignup = function () {
+    $location.path("/signup");
+  };
+  $scope.formData = {}; // Initialize form data object
+  $scope.login = function () {
+    // Login logic
+    console.log("Form data:", $scope.formData);
+  };
 });
--- js/controllers/SignupController.js
+++ js/controllers/SignupController.js
@@ -1,6 +1,28 @@
-app.controller('SignupController', function($scope, $location) {
-    // Controller logic for signup page
-    $scope.goToLogin = function() {
-        $location.path('/');
-    };
+app.controller("SignupController", function ($scope, $location) {
+  // Controller logic for signup page
+  $scope.goToLogin = function () {
+    $location.path("/");
+  };
+  $scope.formData = {}; // Initialize form data object
+  $scope.confirmPassword;
+  $scope.arePasswordsEqual = true;
+
+  $scope.confirm = function () {
+    if ($scope.formData.password !== $scope.confirmPassword) {
+      $scope.arePasswordsEqual = false;
+      $scope.signupForm.$invalid = true;
+    } else {
+      $scope.arePasswordsEqual = true;
+      $scope.signupForm.$invalid = false;
+    }
+  };
+
+  $scope.submitForm = function () {
+    // Handle form submission here
+    if ($scope.signupForm.$invalid) {
+      return;
+    }
+    console.log("Form data:", $scope.formData);
+    // You can perform further validation and submit data to backend API
+  };
 });
